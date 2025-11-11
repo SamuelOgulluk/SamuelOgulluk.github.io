70aa9a468cd9e24a6575314e418ba196abf7cb4f
samuel SAMUEL <ogulluk.samuel@gmail.com>
2025-11-12 00:02:22 +0100

    Corrige le base path pour le dépôt racine

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/SamuelOgulluk.github.io/', // Remplacez 'mon-repo' par le nom de votre dépôt GitHub
       server: {
         port: 3000,
         host: '0.0.0.0',
